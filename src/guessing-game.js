ff06e990e950f1301f7856c9eedaa79315d908c4
Anna Kudriasheva <anna.averina95@gmail.com>
2017-01-29 18:45:19 +0300

    guessing-game/feature: implement methods for guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._min = null;
+        this._max = null;
+        this.num_arr = [];
+    }
 
     setRange(min, max) {
-
+        for(var i = min; i <= max; i++)
+            this.num_arr.push(i);
+        this._min = 1;
+        this._max = this.num_arr.length - 2;
     }
 
     guess() {
-
+        this.bin_num = Math.round((this._min + this._max) / 2);
+        return this.num_arr[this.bin_num];
     }
 
     lower() {
-
+        this._max = this.bin_num - 1;
     }
 
     greater() {
-
+        this._min = this.bin_num + 1;
     }
 }
 
